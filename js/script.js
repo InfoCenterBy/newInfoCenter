71a23eba51b357b0f2fdf7b64580819174690530
Egor <94043508+SVENmods@users.noreply.github.com>
2025-01-24 08:57:26 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -147,22 +147,25 @@ if (agreeDeleteCheck) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-	const radioGroups = document.querySelectorAll('.radio-group-step');
+	const radioGroupsSideNav = document.querySelectorAll('.side-nav-step .radio-group-step');
+	const radioGroupsModal = document.querySelectorAll('.modal .radio-group-step');
 
-	for (let i = 1; i < radioGroups.length; i++) {
-		toggleGroupDisabled(radioGroups[i], true);
-	}
+	function toggleGroupProcess(radioGroups) {
+		for (let i = 1; i < radioGroups.length; i++) {
+			toggleGroupDisabled(radioGroups[i], true);
+		}
 
-	radioGroups.forEach((group, index) => {
-		const radios = group.querySelectorAll("input[type='radio']");
-		radios.forEach((radio) => {
-			radio.addEventListener('change', () => {
-				if (radio.checked && index < radioGroups.length - 1) {
-					toggleGroupDisabled(radioGroups[index + 1], false);
-				}
+		radioGroups.forEach((group, index) => {
+			const radios = group.querySelectorAll("input[type='radio']");
+			radios.forEach((radio) => {
+				radio.addEventListener('change', () => {
+					if (radio.checked && index < radioGroups.length - 1) {
+						toggleGroupDisabled(radioGroups[index + 1], false);
+					}
+				});
 			});
 		});
-	});
+	}
 
 	function toggleGroupDisabled(group, isDisabled) {
 		const radios = group.querySelectorAll("input[type='radio']");
@@ -170,4 +173,7 @@ document.addEventListener('DOMContentLoaded', () => {
 			radio.disabled = isDisabled;
 		});
 	}
+
+	toggleGroupProcess(radioGroupsSideNav);
+	toggleGroupProcess(radioGroupsModal);
 });
