2077e9b1677159e0912f2dc33891496dd27bab62
Egor <94043508+SVENmods@users.noreply.github.com>
2025-03-06 09:19:19 +0300

    Add wrapper to all js

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,5 +1,7 @@
-@@include('app.js', {})
-@@include('sliders.js', {})
-@@include('show-password.js', {})
-@@include('agreeDeleteModal.js',{})
-@@include('stepForm.js',{})
+document.addEventListener('DOMContentLoaded', function () {
+     @@include('app.js', {})
+     @@include('sliders.js', {})
+     @@include('show-password.js', {})
+     @@include('agreeDeleteModal.js',{})
+     @@include('stepForm.js',{})
+})
