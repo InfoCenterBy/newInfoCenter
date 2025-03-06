0f9b246d113dd7ec00d17c18a2c830d379ef7c8e
Egor <94043508+SVENmods@users.noreply.github.com>
2025-03-06 12:19:50 +0300

    Revert "Add wrapper to all js"
    This reverts commit 2077e9b1677159e0912f2dc33891496dd27bab62.

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,7 +1,5 @@
-document.addEventListener('DOMContentLoaded', function () {
-     @@include('app.js', {})
-     @@include('sliders.js', {})
-     @@include('show-password.js', {})
-     @@include('agreeDeleteModal.js',{})
-     @@include('stepForm.js',{})
-})
+@@include('app.js', {})
+@@include('sliders.js', {})
+@@include('show-password.js', {})
+@@include('agreeDeleteModal.js',{})
+@@include('stepForm.js',{})
