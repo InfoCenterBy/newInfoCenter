ed4f46b0ce388b944bb1f359734b01a32041db4d
burak@info-center.by <94043508+SVENmods@users.noreply.github.com>
2025-03-24 17:12:11 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
 const tooltipTriggerList = document.querySelectorAll(
      '[data-bs-toggle="tooltip"]'
 );
@@ -24,104 +23,161 @@ document.addEventListener('DOMContentLoaded', function () {
      }
 });
 
+$('.dont-touch-me').on('mouseenter', function (e) {
+     var maxX = $(window).width() - $(this).width();
+     var maxY = $(window).height() - $(this).height();
+     $(this).css({
+          position: 'absolute',
+          zIndex: 1070,
+          left: getRandomInt(0, maxX),
+          top: getRandomInt(0, maxY),
+     });
+});
+
+function getRandomInt(min, max) {
+     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-	const swiperMainBanner = new Swiper('.swiper.main-banner', {
-		direction: 'horizontal',
-		// loop: true,
+     const swiperMainBanner = new Swiper('.swiper.main-banner', {
+          direction: 'horizontal',
+          // loop: true,
 
-		pagination: {
-			el: '.main-banner-wrapper .swiper-pagination',
-			dynamicBullets: true,
-			dynamicMainBullets: 4,
-			clickable: true,
-		},
+          pagination: {
+               el: '.main-banner-wrapper .swiper-pagination',
+               dynamicBullets: true,
+               dynamicMainBullets: 4,
+               clickable: true,
+          },
 
-		navigation: {
-			nextEl: '.main-banner-wrapper .swiper-button-next-custom',
-			prevEl: '.main-banner-wrapper .swiper-button-prev-custom',
-			// disabledClass: 'disabled',
-			// navigationDisabledClass: 'disabled',
-		},
-		allowTouchMove: true,
-		grabCursor: true,
-		// allowSlideNext: false,
-	});
+          navigation: {
+               nextEl: '.main-banner-wrapper .swiper-button-next-custom',
+               prevEl: '.main-banner-wrapper .swiper-button-prev-custom',
+               // disabledClass: 'disabled',
+               // navigationDisabledClass: 'disabled',
+          },
+          allowTouchMove: true,
+          grabCursor: true,
+          // allowSlideNext: false,
+     });
 
-	const swiperMainNav = new Swiper('.swiper.slider-nav', {
-		direction: 'horizontal',
-		loop: true,
-		allowTouchMove: true,
-		// grabCursor: true,
-		// allowSlideNext: false,
-		breakpointsBase: 'window',
-		autoplay: {
-			delay: 4000,
-			pauseOnMouseEnter: true,
-		},
-		breakpoints: {
-			320: {
-				slidesPerView: 2,
-				spaceBetween: 10,
-				loop: true,
-			},
-			576: {
-				slidesPerView: 3,
-				spaceBetween: 10,
-				loop: true,
-			},
-			768: {
-				slidesPerView: 4,
-				spaceBetween: 10,
-				loop: true,
-			},
-			992: {
-				slidesPerView: 7,
-				spaceBetween: 10,
-				loop: true,
-			},
-			1100: {
-				slidesPerView: 9,
-				loop: true,
-				spaceBetween: 20,
-			},
-		},
-	});
+     const swiperMainNav = new Swiper('.swiper.slider-nav', {
+          direction: 'horizontal',
+          loop: true,
+          allowTouchMove: true,
+          // grabCursor: true,
+          // allowSlideNext: false,
+          breakpointsBase: 'window',
+          autoplay: {
+               delay: 4000,
+               pauseOnMouseEnter: true,
+          },
+          breakpoints: {
+               320: {
+                    slidesPerView: 2,
+                    spaceBetween: 10,
+                    loop: true,
+               },
+               576: {
+                    slidesPerView: 3,
+                    spaceBetween: 10,
+                    loop: true,
+               },
+               768: {
+                    slidesPerView: 4,
+                    spaceBetween: 10,
+                    loop: true,
+               },
+               992: {
+                    slidesPerView: 7,
+                    spaceBetween: 10,
+                    loop: true,
+               },
+               1100: {
+                    slidesPerView: 9,
+                    loop: true,
+                    spaceBetween: 20,
+               },
+          },
+     });
 
-	const swiperMainPartner = new Swiper('.swiper.slider-partner', {
-		direction: 'horizontal',
-		loop: true,
-		allowTouchMove: true,
-		autoplay: {
-			delay: 4000,
-			pauseOnMouseEnter: true,
-		},
-		breakpoints: {
-			320: {
-				slidesPerView: 1,
-				spaceBetween: 10,
-			},
-			576: {
-				slidesPerView: 2,
-				spaceBetween: 10,
-			},
-			768: {
-				slidesPerView: 3,
-				spaceBetween: 10,
-			},
-			992: {
-				slidesPerView: 3,
-				spaceBetween: 10,
-			},
-			1200: {
-				slidesPerView: 4,
-				spaceBetween: 12,
-			},
-			1400: {
-				slidesPerView: 5,
-				spaceBetween: 24,
-			},
-		},
-	});
+     const swiperMainPartner = new Swiper('.swiper.slider-partner', {
+          direction: 'horizontal',
+          loop: true,
+          allowTouchMove: true,
+          autoplay: {
+               delay: 4000,
+               pauseOnMouseEnter: true,
+          },
+          breakpoints: {
+               320: {
+                    slidesPerView: 1,
+                    spaceBetween: 10,
+               },
+               576: {
+                    slidesPerView: 2,
+                    spaceBetween: 10,
+               },
+               768: {
+                    slidesPerView: 3,
+                    spaceBetween: 10,
+               },
+               992: {
+                    slidesPerView: 3,
+                    spaceBetween: 10,
+               },
+               1200: {
+                    slidesPerView: 4,
+                    spaceBetween: 12,
+               },
+               1400: {
+                    slidesPerView: 5,
+                    spaceBetween: 24,
+               },
+          },
+     });
+
+     const sliderBannerSM = new Swiper('.slider-banner-sm .swiper', {
+          direction: 'horizontal',
+          // loop: true,
+          allowTouchMove: true,
+          autoplay: {
+               delay: 4000,
+               pauseOnMouseEnter: true,
+          },
+          navigation: {
+               nextEl: '.slider-banner-sm .swiper-button-next-custom',
+               prevEl: '.slider-banner-sm .swiper-button-prev-custom',
+               // disabledClass: 'disabled',
+               // navigationDisabledClass: 'disabled',
+          },
+          breakpoints: {
+               320: {
+                    slidesPerView: 1,
+                    spaceBetween: 10,
+               },
+               576: {
+                    slidesPerView: 1,
+                    spaceBetween: 10,
+               },
+               768: {
+                    slidesPerView: 2,
+                    spaceBetween: 10,
+               },
+               992: {
+                    slidesPerView: 3,
+                    spaceBetween: 10,
+               },
+               1200: {
+                    slidesPerView: 3,
+                    spaceBetween: 12,
+               },
+               1400: {
+                    slidesPerView: 3,
+                    spaceBetween: 24,
+               },
+          },
+     });
 });
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -398,4 +454,3 @@ document.addEventListener('DOMContentLoaded', function () {
      handleModalZIndex();
 });
 
-});
--- js/uploadFile.js
+++
@@ -1,6 +0,0 @@
-document
-     .getElementById('file-input')
-     .addEventListener('change', function (event) {
-          const fileName = event.target.files[0].name;
-          document.getElementById('file-name').textContent = fileName;
-     });
