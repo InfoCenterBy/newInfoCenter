8434c5316b64611d621fd79aedfb53a596096a2d
burak@info-center.by <94043508+SVENmods@users.noreply.github.com>
2025-03-27 11:12:34 +0300

    Delete usless func

--- #src/js/app.js
+++ #src/js/app.js
@@ -23,17 +23,6 @@ document.addEventListener('DOMContentLoaded', function () {
      }
 });
 
-$('.dont-touch-me').on('mouseenter', function (e) {
-     var maxX = $(window).width() - $(this).width();
-     var maxY = $(window).height() - $(this).height();
-     $(this).css({
-          position: 'absolute',
-          zIndex: 1070,
-          left: getRandomInt(0, maxX),
-          top: getRandomInt(0, maxY),
-     });
-});
-
 function getRandomInt(min, max) {
      return Math.floor(Math.random() * (max - min + 1)) + min;
 }
