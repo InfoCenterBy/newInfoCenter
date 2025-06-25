76b46fff020a925e62f087c0eac329269947cccf
Egor <94043508+SVENmods@users.noreply.github.com>
2025-06-25 09:48:00 +0300

    Updates

--- js/addDesiredPosition.js
+++ js/addDesiredPosition.js
@@ -1,18 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const buttons = document.querySelectorAll('.send-resume');
-  const input = document.getElementById('desired-position');
-  console.log(buttons);
-  console.log(input);
+document.addEventListener('DOMContentLoaded', function () {
+	const buttons = document.querySelectorAll('.send-resume');
+	const input = document.getElementById('desired-position');
 
-  buttons.forEach(button => {
-      button.addEventListener('click', function() {
-          const vacancyTitle = this.closest('.accordion-item').querySelector('.vacancy-title').textContent.replace(/\s+/g, ' ').trim()
-          console.log(vacancyTitle);
-
-          input.value = vacancyTitle;
-  console.log(input);
-
-          console.log('Значение input после установки:', input.value);
-      });
-  });
+	buttons.forEach((button) => {
+		button.addEventListener('click', function () {
+			const vacancyTitle = this.closest('.accordion-item')
+				.querySelector('.vacancy-title')
+				.textContent.replace(/\s+/g, ' ')
+				.trim();
+			input.value = vacancyTitle;
+		});
+	});
 });
